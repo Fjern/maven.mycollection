cc8638ce34838a7ce1eae365e1f7c32e3e45191f
fjern <fernando.forte27@gmail.com>
2019-12-17 15:51:29 -0500

    Finished first 2

--- src/main/java/com/github/curriculeon/MyArrayList.java
+++ src/main/java/com/github/curriculeon/MyArrayList.java
@@ -1,10 +1,65 @@
 package com.github.curriculeon;
 
-public class MyArrayList<SomeType> {
+
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class MyArrayList<SomeType> implements MyCollectionInterface<SomeType>{
+    List<SomeType> myArrL = new ArrayList<>();
+
     public MyArrayList() {
 
     }
 
     public MyArrayList(SomeType[] valuesToBePopulatedWith) {
+        for (SomeType elem:valuesToBePopulatedWith) {
+            myArrL.add(elem);
+        }
+    }
+
+    @Override
+    public void add(SomeType objectToAdd) {
+        myArrL.add(objectToAdd);
+    }
+
+    @Override
+    public void remove(SomeType objectToRemove) {
+//        this.myArrL.remove(objectToRemove);
+       Iterator<SomeType> it= this.iterator();
+       while(it.hasNext()){
+           if( it.next().equals(objectToRemove)){
+              it.remove();
+           }
+       }
+
+    }
+
+    @Override
+    public void remove(int indexOfObjectToRemove) {
+                this.myArrL.remove(indexOfObjectToRemove);
+    }
+
+    @Override
+    public SomeType get(int indexOfElement) {
+        return this.myArrL.get(indexOfElement);
+    }
+
+    @Override
+    public Boolean contains(SomeType objectToCheckFor) {
+        return this.myArrL.contains(objectToCheckFor);
+    }
+
+    @Override
+    public Integer size() {
+        return this.myArrL.size();
+    }
+
+    @Override
+    public Iterator<SomeType> iterator() {
+
+        return this.myArrL.iterator();
     }
 }
--- src/main/java/com/github/curriculeon/MyMap.java
+++ src/main/java/com/github/curriculeon/MyMap.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class MyMap<KeyType, ValueType> {
     public MyMap() {
+
     }
 
     public MyMap(List<KeyValue<KeyType, ValueType>> entries) {
